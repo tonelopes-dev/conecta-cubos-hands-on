bc3414883b3f396f06e92968e5fe624e9c181fb0
Tone Lopes <tonelopes.dev@gmail.com>
2024-03-11 15:44:34 -0300

    feat: atualizando as ppasta de auth

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { ApiManagerModule } from './modules/api-manager/api-manager.module';
 import { ConfigModule } from '@nestjs/config';
 
-import { AuthModule } from './auth/auth.module';
+import { AuthModule } from './auth/github/auth.module';
 import { AppService } from './app.service';
 import { AppController } from './app.controller';
 
